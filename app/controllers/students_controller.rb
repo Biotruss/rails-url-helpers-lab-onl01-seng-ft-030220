5534ca6949a11964195ced4b48789460254d2e8b
Craig Christopher Messi <craig.messi@gmail.com>
2020-05-14 21:19:50 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,6 +12,12 @@ class StudentsController < ApplicationController
   def activate
     @student = Student.find(params[:id])
   
+    if @student.active
+       @student.active = false
+     else
+       @student.active = true
+    end
+    @student.active
     @student.save
 
     redirect_to student_path(@student)
