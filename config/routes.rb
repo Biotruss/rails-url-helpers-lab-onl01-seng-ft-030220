0750c89ecdf3e39b3dcee3d139c7c6a952345310
Craig Christopher Messi <craig.messi@gmail.com>
2020-05-14 21:19:20 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   resources :students, only: [:index, :show]
   get "students/:id", to: "students#show"
   get "students/:id/activate", to: "students#activate", as: "activate_student"
-  #get 'students/:id/activate', to: 'students#activate'
 end
